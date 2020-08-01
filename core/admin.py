b952f9bac35c2d377244996b32222ab7c7c3f4e0
Hudeh <henryudeh@gmail.com>
2020-08-01 23:19:57 +0100

    adding admin & test_admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ["name",'email']
+    fieldsets = (
+        (None, {'fields':('email', 'password')}),
+        (_('Personal Info'), {'fields':('name',)}),
+        (_('Permission'),{'fields': ("is_active", "is_superuser","is_staff")}),
+        (_('Important Date'), {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes':('wide,'),
+            'fields':('email','password1','password2')
+        }),
+         
+    )
+admin.site.register(models.User, UserAdmin)
